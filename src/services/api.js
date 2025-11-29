4c59eb38b5bd965ef1681320eef6abd60a90d8e8
Shiva <sivadurganedhuri0007@gmail.com>
2025-11-29 13:16:23 +0530

    Connecting Deployed Backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = 'http://localhost:8080/api'
+const API_BASE = 'https://flashseat-backend.onrender.com/api'  // FIXED: Live URL
 
 const api = axios.create({
   baseURL: API_BASE,
